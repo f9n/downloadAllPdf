c3278bc631b1ce5935dc1930687f0c1291e2d957
pleycpl <h4rvey@protonmail.com>
2017-04-21 10:58:24 +0300

    created main function

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ console.log("[+] Program Starting");
 const domain = "https://www.tutorialspoint.com";
 const url = "https://www.tutorialspoint.com/tutorialslibrary.htm"
 
-request(url, (error, response, html) => {
+function Main(domain, url) {
   let Links = [];
-  if(!error){
-    const $ = cheerio.load(html);
-    var a, title, href, data;
-    $('ul.menu li').each(function(){
-      data = $(this);
-      a = data.children().first();
-      title = a.attr('title');
-      href = a.attr('href');
-      href = domain + href;
-      Links.push({title: title, href: href});
-    });
-  }
-  console.log(Links);
-});
+  console.log('[+] Scraping TutorialsPoint')
+  request(url, (error, response, html) => {
+    if(!error){
+      const $ = cheerio.load(html);
+      var a, title, href, data;
+      $('ul.menu li').each(function(){
+        data = $(this);
+        a = data.children().first();
+        title = a.attr('title');
+        href = a.attr('href');
+        href = domain + href;
+        Links.push({title: title, href: href});
+      });
+    }
+    console.log(Links);
+  });
+}
+
+Main(domain, url);
+
