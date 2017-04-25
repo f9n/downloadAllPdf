c70262673798e1a48957180202b5fe9c4969d33a
pleycpl <h4rvey@protonmail.com>
2017-04-26 01:14:30 +0300

    showing number of downloading and downloaded

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const request = require('request');
 const cheerio = require('cheerio');
 const chalk   = require('chalk');
 const fs      = require('fs');
-const figlet  = require('figlet');
+//const figlet  = require('figlet');
 const shell   = require('shelljs');
 const CFonts  = require('cfonts');
 const logSymbols = require('log-symbols');
+//const ProgressBar = require('progress');
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
 
 const prettyFont = CFonts.render('All|Pdfs!', {
-  font: '3d',        //define the font face
-	align: 'left',        //define text alignment
+  font: '3d',           //define the font face
+  align: 'left',        //define text alignment
 	colors: ['yellow'],   //define all colors
 	background: 'Black',  //define the background color
 	letterSpacing: 1,     //define letter spacing
@@ -29,6 +32,7 @@ console.log(
 */
 
 console.log(chalk.blue(`${logSymbols.info} Program Starting`));
+
 
 const domain = "https://www.tutorialspoint.com";
 const url = "https://www.tutorialspoint.com/tutorialslibrary.htm"
@@ -62,38 +66,65 @@ function Main(domain, url) {
     }
     //console.log(Links);
     console.log(chalk.yellow(`${logSymbols.warning} Downloading Pdfs...`))
-    //downloadFile(Links[0].href)
     //downloadFiles(Links);
     downloadFilesWithSetInterval(Links);
   });
 }
 
 function downloadFile(link) {
-    //console.log(link);
-    //console.log(link.split("/"));
-    //console.log(link.split("/").pop());
-    console.log(logSymbols.success + chalk.green(` Downloaded  Title: ${chalk.red(link.title)} Link: ${link.href}`))
-    intervalId = setInterval(function() { process.stdout.write('.'); }, 1000);
-    request(link.href)
-      .pipe(fs.createWriteStream(link.href.split("/").pop()))
-      .on('close', function(err) {
-        process.stdout.write('done!\n');
-        clearInterval(intervalId);
-      });
+  eventEmitter.emit('downloading', link)
+  request
+    .get(link.href)
+    .on('response', function(resp) {
+      //console.log(resp.statusCode);
+      //console.log(resp.headers["content-type"]);
+    })
+    .on('data', function(chunk) {
+    })
+    .pipe(fs.createWriteStream('./pdfs/' + link.href.split("/").pop()))
+    .on('close', function(err) {
+      eventEmitter.emit('downloaded', link);
+    });
 }
+
+function downloadFilesWithSetInterval(links) {
+  var i = 0;
+  var j = 0;
+  eventEmitter.on('downloading', function(link) {
+    i++;
+    console.log(chalk.cyan(`[All]: ${links.length} [Downloading]: ${i}  [Downloaded]: ${j} `) +
+      logSymbols.warning + chalk.yellow(` Downloading  Title: ${chalk.red(link.title)} Link: ${link.href}`))
+  })
+  eventEmitter.on('downloaded', function(link) {
+    j++;
+    console.log(chalk.cyan(`[All]: ${links.length} [Downloading]: ${i}  [Downloaded]: ${j} `) +
+      logSymbols.success + chalk.green(` Downloaded   Title: ${chalk.red(link.title)} Link: ${link.href}`));
+  })
+  setInterval(function() {
+    downloadFile(links[i]);
+  }, 8000 );
+}
+
+
+/*
 function downloadFiles(links) {
     for(let link of links) {
         downloadFile(link.href);
     }
 }
-
-function downloadFilesWithSetInterval(links) {
-  var i = 0;
-  setInterval(function() {
-    downloadFile(links[i]);
-    i++;
-  }, 8000 );
+*/
+/*
+function downloadFilesSync(links) {
+  var len = parseInt(resp.headers["content-length"], 10);
+  bar = new ProgressBar(`Downloading ${link.title} [:bar] :rate/bps :percent :etas`, {
+    complete: '=',
+    incomplete: ' ',
+    width: 20,
+    total: len
+  });
+  bar.tick(chunk.length);
 }
+*/
 
 Main(domain, url);
 
